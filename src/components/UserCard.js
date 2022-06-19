a79d92ac878f9bc643e6b58266bf3f0a0f0d17f1
ShareefRahat <imr4h47@gmail.com>
2022-06-19 18:32:18 +0600

    infinite scroll with spinner is added

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const UserCard = ({ randomUser }) => {
-  return <div>UserCard</div>;
+  const { name, picture } = randomUser;
+  return (
+    <>
+      <div className="flex flex-row justify-center items-center w-fit gap-4 p-4">
+        <div>
+          <img
+            className="w-12 rounded-full aspect-square"
+            src={picture?.thumbnail}
+            alt=""
+          />
+        </div>
+        <div>
+          <h4 className="text-md lg:text-lg">
+            {name?.first} {name?.last}
+          </h4>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Header from "../components/Header";
+import Loader from "../components/Loader";
+import UserCard from "../components/UserCard";
 
-const Home = ({ user, setUser }) => {
+const Home = ({ setUser }) => {
+  const [amount, setAmount] = useState(8);
+  const [randomUsers, setRandomUsers] = useState([]);
+  const [totalResults, setTotalResults] = useState(0);
+
+  const url = `https://randomuser.me/api/?results=${amount}`;
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setRandomUsers(data?.results));
+  }, [url, amount]);
+
   const handleLogout = () => {
     setUser(false);
     const userData = { user: "foo", login: false };
     localStorage.setItem("userData", JSON.stringify(userData));
   };
 
+  const fetchMoreData = () => {
+    setAmount(amount + 1);
+    setTotalResults(totalResults + 1);
+  };
+
   return (
-    <div>
-      Home Page
-      <button
-        className="bg-blue-700 p-4 block my-10 text-white text-center"
-        onClick={handleLogout}
-      >
-        LoginOut
-      </button>
-      <div className="my-20 h-[400px] overflow-auto w-fit px-20 mx-auto"></div>
-    </div>
+    <>
+      <section className="mb-10">
+        <Header handleLogout={handleLogout}></Header>
+      </section>
+
+      <h1 className="text-center mb-10 text-lg lg:text-xl text-slate-800 font-bold">
+        Random User Infinite Scroll
+      </h1>
+      <section className="border border-blue-700 lg:w-fit mx-4 lg:mx-auto shadow-lg rounded-md">
+        <InfiniteScroll
+          dataLength={randomUsers?.length}
+          next={fetchMoreData}
+          hasMore={randomUsers?.length !== totalResults}
+          loader={
+            <>
+              <Loader />
+            </>
+          }
+        >
+          <div className="w-fit mx-auto px-6">
+            {!randomUsers ? (
+              <Loader></Loader>
+            ) : (
+              <>
+                {randomUsers.map((randomUser, index) => (
+                  <UserCard key={index} randomUser={randomUser}></UserCard>
+                ))}
+              </>
+            )}
+          </div>
+        </InfiniteScroll>
+      </section>
+    </>
   );
 };
 export default Home;
